bcad8f36cbc238fc8b43a38fdbe49de2629f1800
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2019-09-04 22:55:41 +0800

    Added some simple animations

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { useStoreState } from "easy-peasy";
-
+import { useTransition, animated, config } from "react-spring";
 import { Timer, Control } from "../../components";
 
 function Main() {
   const showConfig = useStoreState(({ nav }) => nav.showConfig);
-  console.log(showConfig);
-  return (
-    <div className="main">
-      <div className="main__body">
-        <Timer />
-        <Control />
-      </div>
-    </div>
+  const transition = useTransition(!showConfig, null, {
+    from: { opacity: 0, transform: "translate3d(-50px, 0, 0)" },
+    enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+    leave: { opacity: 0, transform: "translate3d(-50px, 0, 0)" },
+    config: config.wobbly
+  });
+  return transition.map(
+    ({ item, key, props }) =>
+      item && (
+        <animated.div className="main" key={key} style={props}>
+          <Timer />
+          <Control />
+        </animated.div>
+      )
   );
 }
 
