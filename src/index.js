05a35058dc868c3c2f4deb7564df80222b4bea63
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2019-09-12 20:06:59 +0800

    Added Countdown Animation
    Modified Timer Components and Created Custom Hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./styles/main.scss";
+import "./stylesheets/main.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
