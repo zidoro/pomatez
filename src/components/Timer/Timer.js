f7a0dcc4ce33f6f742cbc682a46db0040fff54e4
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2019-09-17 20:58:23 +0800

    Configured Timer Component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { StoreContext, SET_DURATION, SET_COUNTDOWN } from "../../models";
 import { CountDown, Progress } from "./elements";
 
 function Timer() {
+  const [{ duration, countDown, dashOffset }, dispatch] = useContext(
+    StoreContext
+  ).timer;
+
+  useEffect(() => {
+    dispatch({ type: SET_DURATION, payload: 5 });
+    dispatch({ type: SET_COUNTDOWN, payload: duration });
+  }, [dispatch, duration]);
+
   return (
     <div className="timer">
       <div className="timer__counter">
-        <Progress />
-        <CountDown timerType="Work" />
+        <Progress dashOffset={dashOffset} />
+        <CountDown duration={countDown} timerType="Work" />
       </div>
     </div>
   );
