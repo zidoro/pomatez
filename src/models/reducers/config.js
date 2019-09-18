ff5e5a05b9142aba15929d6b8f10032593126b3e
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2019-09-18 12:48:19 +0800

    Added global config state for TimerConfig and modified the Slider component to get the exact result

---
+++ src/models/reducers/config.js
@@ -0,0 +1,50 @@
+import {
+  SET_WORK_TIME,
+  SET_SHORT_BREAK,
+  SET_LONG_BREAK,
+  SET_SESSION_ROUNDS,
+  RESTORE_DEFAULT
+} from "../actions";
+
+const configState = {
+  workingTime: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  sessionRounds: 4
+};
+
+const configReducer = (state, action) => {
+  switch (action.type) {
+    case SET_WORK_TIME:
+      return {
+        ...state,
+        workingTime: action.payload
+      };
+    case SET_SHORT_BREAK:
+      return {
+        ...state,
+        shortBreak: action.payload
+      };
+    case SET_LONG_BREAK:
+      return {
+        ...state,
+        longBreak: action.payload
+      };
+    case SET_SESSION_ROUNDS:
+      return {
+        ...state,
+        sessionRounds: action.payload
+      };
+    case RESTORE_DEFAULT:
+      return {
+        workingTime: 25,
+        shortBreak: 5,
+        longBreak: 15,
+        sessionRounds: 4
+      };
+    default:
+      return state;
+  }
+};
+
+export { configReducer, configState };
