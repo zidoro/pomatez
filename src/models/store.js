3bd7fb45e20c32a3bf0158ffbec1b485feb26e1e
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2019-09-18 12:41:04 +0800

    Added global config state for TimerConfig and modified the Slider component to get the exact result

--- src/models/store.js
+++ src/models/store.js
@@ -7,7 +7,9 @@ import {
   controlReducer,
   controlState,
   settingReducer,
-  settingState
+  settingState,
+  configReducer,
+  configState
 } from "./reducers";
 
 const StoreContext = React.createContext();
@@ -15,10 +17,11 @@ const StoreContext = React.createContext();
 const Provider = ({ children }) => {
   const nav = useReducer(navReducer, navState);
   const timer = useReducer(timerReducer, timerState);
+  const config = useReducer(configReducer, configState);
   const control = useReducer(controlReducer, controlState);
   const setting = useReducer(settingReducer, settingState);
   return (
-    <StoreContext.Provider value={{ nav, timer, control, setting }}>
+    <StoreContext.Provider value={{ nav, timer, control, config, setting }}>
       {children}
     </StoreContext.Provider>
   );
