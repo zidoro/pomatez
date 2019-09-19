f0260a3cc354b3331c409cb0bc989bd6d7d1a43d
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2019-09-19 12:18:05 +0800

    Solved Mounting and Unmounting problem of Config and Counter containers.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,22 +1,26 @@
 import React, { useContext, useEffect } from "react";
-import { StoreContext, SET_DURATION, SET_COUNTDOWN } from "../../models";
+import { StoreContext, SET_DURATION } from "../../models";
 import { CountDown, Progress } from "./elements";
 
 function Timer() {
-  const [{ duration, countDown, dashOffset }, dispatch] = useContext(
-    StoreContext
-  ).timer;
+  const [{ workingTime }] = useContext(StoreContext).config;
 
-  useEffect(() => {
-    dispatch({ type: SET_DURATION, payload: 5 });
-    dispatch({ type: SET_COUNTDOWN, payload: duration });
-  }, [dispatch, duration]);
+  const [{ duration, dashOffset }, dispatch] = useContext(StoreContext).timer;
+
+  useEffect(
+    () =>
+      dispatch({
+        type: SET_DURATION,
+        payload: workingTime
+      }),
+    [dispatch, workingTime]
+  );
 
   return (
     <div className="timer">
       <div className="timer__counter">
         <Progress dashOffset={dashOffset} />
-        <CountDown duration={countDown} timerType="Work" />
+        <CountDown duration={duration} timerType="Work" />
       </div>
     </div>
   );
