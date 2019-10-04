30e10c7c51f43088ed795cf50ad938ec0eab1956
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2019-10-04 19:26:41 +0800

    Update store.js
    Save data to localStorage for config and settings.

--- src/models/store.js
+++ src/models/store.js
@@ -15,11 +15,33 @@ import {
 const StoreContext = React.createContext();
 
 const Provider = ({ children }) => {
+  let store = JSON.parse(localStorage.getItem("store"));
+
   const nav = useReducer(navReducer, navState);
-  const timer = useReducer(timerReducer, timerState);
-  const config = useReducer(configReducer, configState);
+  const timer = useReducer(
+    timerReducer,
+    store.timer ? store.timer : timerState
+  );
+  const config = useReducer(
+    configReducer,
+    store.config ? store.config : configState
+  );
   const control = useReducer(controlReducer, controlState);
-  const setting = useReducer(settingReducer, settingState);
+  const setting = useReducer(
+    settingReducer,
+    store.setting ? store.setting : settingState
+  );
+
+  localStorage.setItem(
+    "store",
+    JSON.stringify({
+      timer: timer[0],
+      config: config[0],
+      control: control[0],
+      setting: setting[0]
+    })
+  );
+
   return (
     <StoreContext.Provider value={{ nav, timer, control, config, setting }}>
       {children}
