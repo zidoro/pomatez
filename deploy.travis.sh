6d4a31dd0d86b9f196949720c0ff6c7425dfa925
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2020-02-06 22:48:14 +0800

    Update deploy.travis.sh

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ "$TRAVIS_OS_NAME" == osx ]; then
     # deploy on mac and windows
-    yarn release --mac --win
+    GH_TOKEN yarn release --mac --win
 else
     # deploy on linux
     GH_TOKEN yarn release
