4e06128001bfd7ce56f638bf5a118a584affe72b
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2020-02-06 19:34:16 +0800

    Change .travis.yml configuration

--- deploy.travis.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [ "$TRAVIS_OS_NAME" == osx ]; then
-    # deploy on mac
-    cd app && yarn release
-else
-    # deploy on windows and linux
-    cd app && docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux --win"
-fi
