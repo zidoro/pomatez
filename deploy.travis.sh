c0e9e0f5b3cd592fc9c5d6e01fea0bd790b55348
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-02-06 22:58:34 +0800

    Try to make .travis.yml config works

--- deploy.travis.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [ "$TRAVIS_OS_NAME" == osx ]; then
-    # deploy on mac and windows
-    GH_TOKEN yarn release --mac --win
-else
-    # deploy on linux
-    GH_TOKEN yarn release
-fi
