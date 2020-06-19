a8270a28dd6f1bf071e4005e6b79d7d75cba472a
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-06-19 12:53:56 +0800

    chore: remove some javascript files that aren't necessary from being uploaded

--- app/public/helpers/isWindow.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isWindow = void 0;
-exports.isWindow = function () { return process.platform === "win32"; };
--- app/public/store/schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var schema = {
-    useNativeTitlebar: { type: "string" },
-};
-exports.default = schema;
