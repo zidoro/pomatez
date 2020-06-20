8665bb420fcc1a9f6e8763b1ad8dbb9ed507fbea
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-06-20 16:01:04 +0800

    fix: fixed switching titlebar bug in linux operating system

--- app/electron/helpers/getIcon.ts
+++ app/electron/helpers/getIcon.ts
@@ -3,19 +3,10 @@ import path from "path";
 export function getIcon() {
   switch (process.platform) {
     case "darwin":
-      return path.join(
-        __dirname,
-        "../../src/assets/logos/logo-transparent@2x.png"
-      );
+      return path.join(__dirname, "../../src/assets/logos/logo-dark@2x.png");
     case "linux":
-      return path.join(
-        __dirname,
-        "../../src/assets/logos/logo-transparent.png"
-      );
+      return path.join(__dirname, "../../src/assets/logos/logo-dark.png");
     default:
-      return path.join(
-        __dirname,
-        "../../src/assets/logos/logo-transparent.ico"
-      );
+      return path.join(__dirname, "../../src/assets/logos/logo-dark.ico");
   }
 }
--- app/electron/store/index.ts
+++ app/electron/store/index.ts
@@ -1,5 +1,20 @@
 import Store from "electron-store";
+import { systemPreferences } from "electron";
+import { isWindow } from "../helpers";
 
-const store = new Store();
+type StoreProps = {
+  isDarkMode: boolean;
+  useNativeTitlebar: boolean;
+};
+
+const store = new Store<StoreProps>();
+
+if (store.get("isDarkMode") == null) {
+  store.set("isDarkMode", systemPreferences.isDarkMode());
+}
+
+if (store.get("useNativeTitlebar") == null) {
+  store.set("useNativeTitlebar", isWindow() ? false : true);
+}
 
 export default store;
