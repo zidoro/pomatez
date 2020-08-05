ee0638564cb9f7735478f0cd76f136306035156d
roldanjrCodeArts9711 <roldanjrmontilla@gmail.com>
2020-08-05 23:12:04 +0800

    perf: decreased cpu usage on break while keeping the screen awake using use-stay-awake hook"

--- app/src/hooks/index.ts
+++ app/src/hooks/index.ts
@@ -2,5 +2,4 @@ export * from "./useInputHandler";
 export * from "./useTargetOutside";
 export * from "./useRippleEffect";
 export * from "./useNotification";
-export * from "./useSleepMode";
 export * from "./useTime";
