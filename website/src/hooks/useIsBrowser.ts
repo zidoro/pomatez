fb9f111b65737fc5d6f317704618df819d8cc7f3
Andrew Gillis <gillis.andrew@gmail.com>
2022-02-25 16:00:20 -0500

    fix(website): force render on client-side (#206)

---
+++ website/src/hooks/useIsBrowser.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useIsBrowser = () => {
+	const [isBrowser, setIsBrowser] = useState(false);
+
+	useEffect(() => {
+		setIsBrowser(true);
+		return () => setIsBrowser(false);
+	}, []);
+
+	return isBrowser;
+};
