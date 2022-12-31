2e8faf9841d09db74e783b356d6ca14a94340ee6
Sekwah <contact@sekwah.com>
2022-12-31 16:21:30 +0000

    feat(tauri): pass the data from the frontend  into the commands

--- app/tauri/src/commands.rs
+++ app/tauri/src/commands.rs
@@ -1,55 +1,50 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-use tauri::{Builder, Wry};
+use tauri::{Builder, Runtime, Wry};
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_minimize<R: Runtime>(minimize_to_tray: bool, _window: tauri::Window<R>) {
+    println!("Minimize! {}", minimize_to_tray.to_string().as_str());
 }
 
 #[tauri::command]
-fn set_minimize() {
-    println!("Minimize!");
-}
-
-#[tauri::command]
-fn set_close() {
+fn set_close<R: Runtime>(_window: tauri::Window<R>) {
     println!("set_close!");
 }
 
 #[tauri::command]
-fn set_show() {
+fn set_show<R: Runtime>(_window: tauri::Window<R>) {
     println!("set_show!");
 }
 
 #[tauri::command]
-fn set_always_on_top() {
-    println!("set_always_on_top!");
+fn set_always_on_top<R: Runtime>(always_on_top: bool, _window: tauri::Window<R>) {
+    println!("set_always_on_top! {}", always_on_top);
 }
 
 #[tauri::command]
-fn set_fullscreen_break() {
-    println!("set_fullscreen_break!");
+fn set_fullscreen_break<R: Runtime>(should_fullscreen: bool, always_on_top: bool, _window: tauri::Window<R>) {
+    println!("set_fullscreen_break! {} {}", should_fullscreen, always_on_top);
 }
 
 #[tauri::command]
-fn set_compact_mode() {
-    println!("set_compact_mode!");
+fn set_compact_mode<R: Runtime>(compact_mode: bool, _window: tauri::Window<R>) {
+    println!("set_compact_mode! {}", compact_mode);
 }
 
 #[tauri::command]
-fn set_ui_theme() {
-    println!("set_ui_theme!");
+fn set_ui_theme<R: Runtime>(is_dark_mode: bool, _window: tauri::Window<R>) {
+    println!("set_ui_theme! {}", is_dark_mode);
 }
 
 #[tauri::command]
-fn set_native_titlebar() {
-    println!("set_native_titlebar!");
+fn set_native_titlebar<R: Runtime>(use_native_titlebar: bool, _window: tauri::Window<R>) {
+    println!("set_native_titlebar! {}", use_native_titlebar);
 }
 
 #[tauri::command]
-fn tray_icon_update() {
-    println!("tray_icon_update!");
+fn tray_icon_update<R: Runtime>(data_url: String, _window: tauri::Window<R>) {
+    println!("tray_icon_update! {}", data_url);
 }
 
 /**
@@ -65,7 +60,7 @@ pub trait CommandRegister {
 
 impl CommandRegister for Builder<Wry> {
     fn register_pomatez_commands(self) -> tauri::Builder<Wry> {
-        self.invoke_handler(tauri::generate_handler![greet, set_show, set_always_on_top,
+        self.invoke_handler(tauri::generate_handler![set_show, set_always_on_top,
             set_fullscreen_break, set_compact_mode, set_ui_theme, set_native_titlebar,
             tray_icon_update, set_close, set_minimize])
     }
--- app/tauri/src/main.rs
+++ app/tauri/src/main.rs
@@ -6,20 +6,10 @@ windows_subsystem = "windows"
 #[macro_use]
 mod commands;
 
-use tauri::{Manager};
 use commands::CommandRegister;
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(debug_assertions)] // only include this code on debug builds
-            {
-                let window = app.get_window("main").unwrap();
-                window.open_devtools();
-            }
-            Ok(())
-        }).register_pomatez_commands().run(tauri::generate_context!()).expect("error while running tauri application");
+        .register_pomatez_commands().run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
-// fn register_commands<E>(app: Builder<Wry>) -> Builder<Wry> {
-//     app.invoke_handler(tauri::generate_handler![greet])
-// }
