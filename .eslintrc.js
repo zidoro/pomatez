dc6f2c198db368a4cee8eec309c1b2a0f681a488
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-02-02 00:42:55 +0800

    :building_construction: Making Architectural Changes

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ["@pomatez/eslint-config"],
+  root: true,
+};
---
+++ apps/main/src/preload.ts
@@ -0,0 +1,28 @@
+import { contextBridge, ipcRenderer, shell } from "electron";
+import { TO_MAIN, FROM_MAIN } from "@pomatez/common-config";
+
+// https://github.com/electron/electron/issues/9920#issuecomment-575839738
+
+contextBridge.exposeInMainWorld("electron", {
+  send: (channel: string, ...args: any[]) => {
+    if (TO_MAIN.includes(channel)) {
+      ipcRenderer.send(channel, ...args);
+    }
+  },
+  recieve: (channel: string, response: (...args: any[]) => void) => {
+    if (FROM_MAIN.includes(channel)) {
+      ipcRenderer.on(
+        channel,
+        (event: Electron.IpcRendererEvent, ...args) => {
+          return response(...args);
+        }
+      );
+    }
+  },
+  openExternal: (
+    url: string,
+    options?: Electron.OpenExternalOptions
+  ) => {
+    shell.openExternal(url);
+  },
+});
