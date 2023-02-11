cb9112f7f2512eb8a6175603951933bff86f9475
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-02-11 22:50:12 +0800

    :construction_worker: UI - Setup `vitest` and write the 1st test suite

---
+++ packages/ui/utils/string/__tests__/cx.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { cx } from "../cx";
+
+describe("cx - class names utility", () => {
+  it("should return empty string", () => {
+    expect(cx()).toBe("");
+  });
+
+  it("should return a string value", () => {
+    expect(cx("foo")).toBe("foo");
+  });
+
+  it("should return a joined string values", () => {
+    expect(cx("foo", "bar")).toBe("foo bar");
+  });
+});
