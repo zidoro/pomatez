c288fa79d896708382e3aad13f5befb0ecfeee6c
przemeksgames <110451792+Mupli@users.noreply.github.com>
2023-05-13 11:53:30 +0200

    Update analytics.ts

--- app/main/src/helpers/analytics.ts
+++ app/main/src/helpers/analytics.ts
@@ -8,8 +8,8 @@ const userId = store.get("userId") || uuid();
 store.set("userId", userId);
 
 function activateUser() {
-  const user = ua("UA-172128342-2", userId);
-  user.pageview("/").send();
+  // const user = ua("UA-172128342-2", userId);
+  // user.pageview("/").send();
 }
 
 export { activateUser };
