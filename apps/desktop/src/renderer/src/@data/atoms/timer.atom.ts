53f1e010b15d1e182617f024dc0ec5a842e58ca3
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-06-30 00:25:17 +0800

    :sparkles: Handle Autostart Break and Work Logic"

--- apps/desktop/src/renderer/src/@data/atoms/timer.atom.ts
+++ apps/desktop/src/renderer/src/@data/atoms/timer.atom.ts
@@ -2,6 +2,7 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { TIMER_PROGRESS_CIRCUMFERENCE } from "@pomatez/ui";
 import { minutesToSeconds } from "@renderer/utils";
+import { settingsAtom } from "./settings.atom";
 import { configAtom } from "./config.atom";
 
 type SessionType = "stayFocused" | "shortBreak" | "longBreak";
@@ -22,6 +23,7 @@ const defaultTimer: TimerProps = {
 
 export const timerAtom = atomWithStorage("timer", defaultTimer);
 
+// reset timer state on every onMount event
 timerAtom.onMount = (set) => () => set(defaultTimer);
 
 export const durationAtom = atomWithStorage("timer.duration", 0);
@@ -40,8 +42,6 @@ const counterAtom = atom<{
 const invokeTimerAction = atom(
   null,
   (get, set, action: "start" | "stop") => {
-    const timer = get(timerAtom);
-
     if (action === "start") {
       // if timer is already running, do nothing
       if (get(counterAtom) !== null) return;
@@ -60,12 +60,7 @@ const invokeTimerAction = atom(
         // if time is up, reset timer
         if (elapsedTime >= get(durationAtom)) {
           setTimeout(() => {
-            set(counterAtom, null);
-            set(timerAtom, {
-              ...timer,
-              isRunning: false,
-            });
-            set(elapsedAtom, 0);
+            set(nextSessionAtom);
           }, 1000);
         } else {
           // otherwise, keep ticking
@@ -128,11 +123,13 @@ export const resetTimerAtom = atom(null, (get, set) => {
 export const nextSessionAtom = atom(null, (get, set) => {
   const timer = get(timerAtom);
   const config = get(configAtom);
+  const settings = get(settingsAtom);
 
   let newDuration = get(durationAtom);
 
   let newSessionType = timer.sessionType;
   let newSessionRound = timer.round;
+  let shouldAutoStart = false;
 
   switch (timer.sessionType) {
     case "stayFocused":
@@ -143,26 +140,42 @@ export const nextSessionAtom = atom(null, (get, set) => {
         newSessionType = "longBreak";
         newDuration = minutesToSeconds(config.longBreak);
       }
+
+      if (settings.autoStartBreak) {
+        shouldAutoStart = true;
+      }
       break;
     case "shortBreak":
     case "longBreak":
       newSessionType = "stayFocused";
+
       newSessionRound =
         timer.round < config.sessionRounds
           ? timer.round + 1
           : defaultTimer.round;
       newDuration = minutesToSeconds(config.stayFocused);
+
+      if (settings.autoStartWork) {
+        shouldAutoStart = true;
+      }
       break;
+    default:
+      newSessionType = "stayFocused";
   }
 
-  set(elapsedAtom, 0);
-  set(counterAtom, null);
-  set(durationAtom, newDuration);
+  set(elapsedAtom, 0); // reset elapsed time first
+  set(invokeTimerAction, "stop"); // stop timer to clear timeout
+  set(durationAtom, newDuration); // update the duration
   set(timerAtom, {
     ...timer,
     round: newSessionRound,
     sessionType: newSessionType,
+    isRunning: shouldAutoStart,
   });
+  // start timer if auto start is enabled
+  if (shouldAutoStart) {
+    set(invokeTimerAction, "start");
+  }
 });
 
 export const resetRoundAtom = atom(null, (get, set) => {
