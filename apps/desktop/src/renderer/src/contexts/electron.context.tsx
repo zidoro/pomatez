aeaa0b2327bc526537d55ba49ba226725d7266cc
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-06-30 22:37:12 +0800

    :adhesive_bandage: Fix Window Re-center Issue during Vite HMR"

--- apps/desktop/src/renderer/src/contexts/electron.context.tsx
+++ apps/desktop/src/renderer/src/contexts/electron.context.tsx
@@ -3,8 +3,9 @@ import {
   ReactNode,
   useCallback,
   useEffect,
+  useMemo,
 } from "react";
-import { useAtomValue, useSetAtom } from "jotai";
+import { atom, useAtomValue, useSetAtom } from "jotai";
 import { runOnElectron } from "@renderer/utils";
 import {
   resetTimerAtom,
@@ -22,9 +23,13 @@ const ElectronContext = createContext<ElectronContextProps>({});
 const ElectronProvider = ({ children }: { children: ReactNode }) => {
   const resetTimer = useSetAtom(resetTimerAtom);
 
-  const settings = useAtomValue(settingsAtom);
+  const settings = useAtomValue(
+    useMemo(() => atom((get) => get(settingsAtom)), [])
+  );
 
-  const timer = useAtomValue(timerAtom);
+  const timer = useAtomValue(
+    useMemo(() => atom((get) => get(timerAtom)), [])
+  );
 
   const onMinimizeWindow = useCallback(() => {
     runOnElectron(() => {
