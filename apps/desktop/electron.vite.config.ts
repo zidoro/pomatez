78d4eff9a540f71fc184237177534f527e89fcfc
Roldan Montilla Jr <roldanjr.dev@gmail.com>
2023-06-30 21:25:43 +0800

    :technologist: Fix Vite HMR Issue using Jotai

--- apps/desktop/electron.vite.config.ts
+++ apps/desktop/electron.vite.config.ts
@@ -1,7 +1,9 @@
-import { resolve } from "path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
+import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 export default defineConfig({
   main: {
@@ -25,6 +27,11 @@ export default defineConfig({
         "@renderer": resolve("src/renderer/src"),
       },
     },
-    plugins: [react(), svgr()],
+    plugins: [
+      react({
+        babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] },
+      }),
+      svgr(),
+    ],
   },
 });
