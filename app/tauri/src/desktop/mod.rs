1e594b29dc74086a6670a00540fcb9916e14cc2c
Sekwah <contact@sekwah.com>
2023-11-13 02:09:59 +0000

    feat: split desktop logic

---
+++ app/tauri/src/desktop/mod.rs
@@ -0,0 +1,5 @@
+// desktop/mod.rs
+pub mod commands;
+pub mod global_shortcuts;
+pub mod system_tray;
+pub mod updater;
--- app/tauri/src/lib.rs
+++ app/tauri/src/lib.rs
@@ -3,38 +3,51 @@
 #[cfg(debug_assertions)]
 use tauri::{Manager};
 use tauri::{RunEvent};
+#[cfg(desktop)]
 use tauri_plugin_autostart::MacosLauncher;
+#[cfg(desktop)]
 use tauri_plugin_window;
 
-#[macro_use]
-mod commands;
-mod system_tray;
-mod global_shortcuts;
-mod updater;
+use tauri_plugin_shell;
 
-use commands::PomatezCommands;
-use system_tray::PomatezTray;
-use global_shortcuts::{PomatezGlobalShortcutsSetup, PomatezGlobalShortcutsRegister};
+#[cfg(desktop)]
+mod desktop;
+
+#[cfg(desktop)]
+use desktop::{
+    commands::PomatezCommands,
+    global_shortcuts::{PomatezGlobalShortcutsRegister, PomatezGlobalShortcutsSetup},
+    system_tray::PomatezTray,
+    updater,
+};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let app = tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
+    let builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    let builder = builder.plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, None))
         .plugin(tauri_plugin_window::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .register_pomatez_commands()
-        .setup(|app| {
-            #[cfg(desktop)]
-            {
+        .plugin(tauri_plugin_updater::Builder::new().build());
+
+    let builder = builder.plugin(tauri_plugin_shell::init());
+
+    #[cfg(desktop)]
+    let builder = builder.register_pomatez_commands();
+
+    #[cfg(desktop)]
+    {
+        let builder = builder
+            .setup(|app| {
                 app.setup_global_shortcuts();
                 app.set_pomatez_system_tray();
-            }
 
-            Ok(())
-        })
-        .build(tauri::generate_context!())
+                Ok(())
+            });
+    }
+
+    let app = builder.build(tauri::generate_context!())
         .expect("error while running tauri application");
 
     app.run(|app_handle, e| match e {
