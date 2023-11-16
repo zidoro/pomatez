9f728b2573ab32d368f482f49e054bd1ba5551fe
Sekwah <contact@sekwah.com>
2023-11-16 18:57:19 +0000

    chore: update config for running website locally

--- website/gatsby-config.ts
+++ website/gatsby-config.ts
@@ -17,31 +17,35 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    {
-      resolve: "gatsby-plugin-google-gtag",
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          ENV.GA_TRACKING_ID, // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: [],
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-        },
-      },
-    },
+    ...(ENV.GA_TRACKING_ID
+      ? [
+          {
+            resolve: "gatsby-plugin-google-gtag",
+            options: {
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                ENV.GA_TRACKING_ID, // Google Analytics / GA
+              ],
+              // This object gets passed directly to the gtag config command
+              // This config will be shared across all trackingIds
+              gtagConfig: {
+                anonymize_ip: true,
+              },
+              // This object is used for configuration specific to this plugin
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: [],
+                // Delays processing pageview events on route update (in milliseconds)
+                delayOnRouteUpdate: 0,
+              },
+            },
+          },
+        ]
+      : []),
     {
       resolve: "gatsby-plugin-styled-components",
       options: {
