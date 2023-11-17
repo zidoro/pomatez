09e5ae39436ffe95b032df12e6c6752cfa704ab1
Sekwah <contact@sekwah.com>
2023-11-17 02:12:37 +0000

    fix(tauri): github link opens in app

--- app/renderer/src/routes/Settings/StickySection.tsx
+++ app/renderer/src/routes/Settings/StickySection.tsx
@@ -4,7 +4,11 @@ import { StyledSectionSticky, StyledStarButton } from "styles";
 const StickySection: React.FC = () => {
   return (
     <StyledSectionSticky>
-      <StyledStarButton as="a" href="https://github.com/zidoro/pomatez">
+      <StyledStarButton
+        as="a"
+        href="https://github.com/zidoro/pomatez"
+        target="_blank"
+      >
         Star it on GitHub
       </StyledStarButton>
     </StyledSectionSticky>
