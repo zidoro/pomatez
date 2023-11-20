a9b17618695a701de3eb230b6b1e43a62ccef512
Sekwah <contact@sekwah.com>
2023-11-20 00:56:07 +0000

    chore: refactored on top of master changes

--- app/tauri/src/lib.rs
+++ app/tauri/src/lib.rs
@@ -1,12 +1,16 @@
-#[cfg(debug_assertions)]
-use tauri::Manager;
-use tauri::RunEvent;
+#![cfg_attr(
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
+)]
+
+#[cfg(desktop)]
+use tauri::{Manager};
+use tauri::{RunEvent};
 #[cfg(desktop)]
 use tauri_plugin_autostart::MacosLauncher;
 #[cfg(desktop)]
+use tauri_plugin_updater::Builder;
 use tauri_plugin_window;
-
-use tauri_plugin_shell;
 
 #[cfg(desktop)]
 mod desktop;
@@ -16,48 +20,75 @@ use desktop::{
     commands::PomatezCommands,
     global_shortcuts::{PomatezGlobalShortcutsRegister, PomatezGlobalShortcutsSetup},
     system_tray::PomatezTray,
-    updater,
 };
+
+#[cfg(target_os = "macos")]
+use crate::commands::try_set_native_titlebar;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = tauri::Builder::default();
+    let mut context = tauri::generate_context!();
 
     #[cfg(desktop)]
-    let builder = builder
-        .plugin(tauri_plugin_autostart::init(
+    let config = context.config_mut();
+
+    let app = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    let app = app.plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             None,
-        ))
-        .plugin(tauri_plugin_window::init())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_updater::Builder::new().build());
+        ));
 
-    let builder = builder.plugin(tauri_plugin_shell::init());
-
-    #[cfg(desktop)]
-    let builder = builder.register_pomatez_commands();
+    let app = app.plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_notification::init());
 
     #[cfg(desktop)]
-    let builder = builder.setup(|app| {
-        app.setup_global_shortcuts();
-        app.set_pomatez_system_tray();
+    let mut app = app;
 
-        Ok(())
-    });
+    #[cfg(desktop)]
+    if config.tauri.bundle.updater.active {
+        app = app.plugin(Builder::new().build());
+    }
 
-    let app = builder
-        .build(tauri::generate_context!())
+    #[cfg(desktop)]
+    let app = app.register_pomatez_commands();
+
+    #[allow(unused_variables)]
+    let app = app.setup(|app| {
+            #[cfg(desktop)]
+            {
+                app.setup_global_shortcuts();
+                app.set_pomatez_system_tray();
+            }
+
+            Ok(())
+        });
+
+    let app = app.build(context)
         .expect("error while running tauri application");
 
     #[allow(unused_variables)]
     app.run(|app_handle, e| match e {
         RunEvent::Ready => {
             #[cfg(desktop)]
-            app_handle.register_global_shortcuts();
+            {
+                app_handle.register_global_shortcuts();
 
-            #[cfg(debug_assertions)]
-            app_handle.get_window("main").unwrap().open_devtools();
+                let window = app_handle.get_window("main").unwrap();
+
+                // There is a bug on mac where the size is not properly respected initially, though this seems to fix it.
+                #[cfg(target_os = "macos")]
+                {
+                    try_set_native_titlebar(false, &window);
+                    try_set_native_titlebar(true, &window);
+                }
+
+                #[cfg(debug_assertions)]
+                window.open_devtools();
+            }
+
 
             println!("Pomatez is ready");
         }
--- app/tauri/src/main.rs
+++ app/tauri/src/main.rs
@@ -1,79 +1,8 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
-use tauri::{Manager};
-use tauri::{RunEvent};
-use tauri_plugin_autostart::MacosLauncher;
-use tauri_plugin_window;
-
-#[macro_use]
-mod commands;
-mod global_shortcuts;
-mod system_tray;
-mod updater;
-
-#[cfg(target_os = "macos")]
-use crate::commands::try_set_native_titlebar;
-
-use commands::PomatezCommands;
-use global_shortcuts::{PomatezGlobalShortcutsRegister, PomatezGlobalShortcutsSetup};
-use system_tray::PomatezTray;
-
 fn main() {
-    let mut context = tauri::generate_context!();
-
-    let config = context.config_mut();
-
-    let mut app = tauri::Builder::default()
-        .plugin(tauri_plugin_autostart::init(
-            MacosLauncher::LaunchAgent,
-            None,
-        ))
-        .plugin(tauri_plugin_window::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_notification::init());
-
-
-
-    if config.tauri.bundle.updater.active {
-        app = app.plugin(tauri_plugin_updater::Builder::new().build());
-    }
-
-    let app = app
-        .register_pomatez_commands()
-        .setup(|app| {
-            #[cfg(desktop)]
-            {
-                app.setup_global_shortcuts();
-                app.set_pomatez_system_tray();
-            }
-
-            Ok(())
-        })
-        .build(context)
-        .expect("error while running tauri application");
-
-    #[cfg(desktop)]
-    app.run(|app_handle, e| match e {
-        RunEvent::Ready => {
-            app_handle.register_global_shortcuts();
-
-            let window = app_handle.get_window("main").unwrap();
-
-            // There is a bug on mac where the size is not properly respected initially, though this seems to fix it.
-            #[cfg(target_os = "macos")]
-            {
-                try_set_native_titlebar(false, &window);
-                try_set_native_titlebar(true, &window);
-            }
-
-            #[cfg(debug_assertions)]
-            window.open_devtools();
-
-            println!("Pomatez is ready");
-        }
-        _ => {}
-    });
+    pomatez_lib::run()
 }
