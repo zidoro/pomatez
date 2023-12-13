73158541f0eb453d8a82c24d495be68b2323d06e
sekwah <contact@sekwah.com>
2023-12-13 04:38:20 +0000

    chore: fix electron signing

--- app/electron/electron-builder/afterSign.js
+++ app/electron/electron-builder/afterSign.js
@@ -3,6 +3,7 @@ const { notarize } = require("electron-notarize");
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== "darwin" || !process.env.API_KEY_ID) {
+    console.log("Skipping notarization");
     return;
   }
 
@@ -14,7 +15,7 @@ exports.default = async function notarizing(context) {
     tool: "notarytool",
     appBundleId: "com.roldanjr.pomatez",
     appPath: `${appOutDir}/${appName}.app`,
-    appleApiKey: `~/private_keys/AuthKey_${process.env.API_KEY_ID}.p8`,
+    appleApiKey: process.env.APPLE_API_KEY_PATH,
     appleApiKeyId: process.env.API_KEY_ID,
     appleApiIssuer: process.env.API_KEY_ISSUER_ID,
   });
