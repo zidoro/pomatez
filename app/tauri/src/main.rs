c44f8b7ba57307aefab8787735b7c7000178cac4
Sekwah <contact@sekwah.com>
2026-05-18 02:23:56 +0100

    fix(tauri): add webkit flag to set on linux to stop crashes on wayland

--- app/tauri/src/main.rs
+++ app/tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    // Workaround for WebKitGTK DMABUF renderer crash on Wayland
+    // (Gdk Error 71, WebLoaderStrategy internallyFailedLoadTimerFired)
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     pomatez_lib::run();
 }
